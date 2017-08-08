fc53bc6e30b269f462c30cf29f88c56653267909
adeer <deer0013@gmail.com>
2017-08-08 16:27:17 -0400

    class changes

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ console.log("secret msg");
 function change(){
 	document.getElementById('el').innerHTML ="You Clicked";
 }
+
+var first = 100;
+var second = 200;
+console.log( first + second);
+
+function addText(){
+	var string1 = document.getElementById('string1').value;
+	var string2 = document.getElementById('string2').value;
+	document.getElementById('result').innerHTML=string1 + string2;
+
+}
+
+var MyInfo = {
+	First: 'Adam', 
+	Last: 'Deer', 
+	Birth: 'March19',
+	Interests:["hockey", "internetz", "dogs"]
+};
+
+var MyInterest = ["hockey", "internetz", "dogs"];
+
+console.log(MyInfo['Last']);
+console.log(MyInterest[1]);
+console.log(MyInterest[0], MyInfo['First']);
+console.log(MyInterest[2], MyInfo['Birth']);
+console.log(MyInfo.Interests[1]);
+
